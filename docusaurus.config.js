ce59867a8a17c3352f9cda8662cc1634a3e53ddf
SabCampos <s.campos@qairadrones.com>
2020-08-26 22:55:11 -0500

    docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'Documentation-OpenSource',
   tagline: 'The tagline of my site',
-  url: ' https://github.com/qAIRa/Documentation-OpenSource/',
+  url: ' /Documentation-OpenSource/',
   baseUrl: '/Documentation-OpenSource/',
   onBrokenLinks: 'throw',
   favicon: 'img/favicon.ico',
