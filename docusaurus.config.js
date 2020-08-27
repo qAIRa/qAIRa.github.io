103dcdd8b2f9618d90c5d3761de2c9777c758c84
SabCampos <s.campos@qairadrones.com>
2020-08-26 22:40:18 -0500

    docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,78 +7,78 @@ module.exports = {
   favicon: 'img/favicon.ico',
   organizationName: 'qAIRa', // Usually your GitHub org/user name.
   projectName: 'Documentation-OpenSource', // Usually your repo name.
-  themeConfig: {
-    navbar: {
-      title: 'My Site',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
-        },
-        {to: 'blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Style Guide',
-              to: 'docs/',
-            },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-    },
-  },
+  // themeConfig: {
+  //   navbar: {
+  //     title: 'My Site',
+  //     logo: {
+  //       alt: 'My Site Logo',
+  //       src: 'img/logo.svg',
+  //     },
+  //     items: [
+  //       {
+  //         to: 'docs/',
+  //         activeBasePath: 'docs',
+  //         label: 'Docs',
+  //         position: 'left',
+  //       },
+  //       {to: 'blog', label: 'Blog', position: 'left'},
+  //       {
+  //         href: 'https://github.com/facebook/docusaurus',
+  //         label: 'GitHub',
+  //         position: 'right',
+  //       },
+  //     ],
+  //   },
+  //   footer: {
+  //     style: 'dark',
+  //     links: [
+  //       {
+  //         title: 'Docs',
+  //         items: [
+  //           {
+  //             label: 'Style Guide',
+  //             to: 'docs/',
+  //           },
+  //           {
+  //             label: 'Second Doc',
+  //             to: 'docs/doc2/',
+  //           },
+  //         ],
+  //       },
+  //       {
+  //         title: 'Community',
+  //         items: [
+  //           {
+  //             label: 'Stack Overflow',
+  //             href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+  //           },
+  //           {
+  //             label: 'Discord',
+  //             href: 'https://discordapp.com/invite/docusaurus',
+  //           },
+  //           {
+  //             label: 'Twitter',
+  //             href: 'https://twitter.com/docusaurus',
+  //           },
+  //         ],
+  //       },
+  //       {
+  //         title: 'More',
+  //         items: [
+  //           {
+  //             label: 'Blog',
+  //             to: 'blog',
+  //           },
+  //           {
+  //             label: 'GitHub',
+  //             href: 'https://github.com/facebook/docusaurus',
+  //           },
+  //         ],
+  //       },
+  //     ],
+  //     copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+  //   },
+  // },
   presets: [
     [
       '@docusaurus/preset-classic',
