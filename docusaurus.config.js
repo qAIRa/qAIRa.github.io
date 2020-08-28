716e829ce2c4cedfca7e9b8d64b61de70ae9b6e2
SabCampos <s.campos@qairadrones.com>
2020-08-28 11:05:40 -0500

    test branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -85,7 +85,7 @@ module.exports = {
       {
         docs: {
           // It is recommended to set document id as docs home page (`docs/` path).
-          homePageId: 'doc1',
+          homePageId: 'docs/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
