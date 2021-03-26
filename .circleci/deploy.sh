3f1150d4d46e34d9c1c352f617940516558ab356
SabCampos <s.campos@qairadrones.com>
2021-03-26 16:35:17 -0500

    path to deploy

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -12,7 +12,7 @@ ssh-keyscan github.com >> ~/.ssh/known_hosts
 
 # stage generated HTML for GitHub Pages
 git clone --quiet --branch=gh-pages $CIRCLE_REPOSITORY_URL $DEPLOY_DIR
-rsync --archive --recursive --verbose --remove-source-files $HOME/project/* $DEPLOY_DIR
+rsync --archive --recursive --verbose --remove-source-files $HOME/project/build/* $DEPLOY_DIR
 
 # git client setup
 cd $DEPLOY_DIR
